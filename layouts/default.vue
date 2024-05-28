68997cd722f0e8fc0411deb2fe8cc9464dab7f66
bruno.bhab <bruno.henrique@beedoo.com.br>
2024-05-28 15:48:39 -0300

    define rotas autenticadas

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,16 +4,6 @@
 
 <template>
     <div>
-        <h1>X</h1>
-        <ul>
-            <li>
-                <NuxtLink href="/">Home</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink href="/about">About</NuxtLink>
-            </li>
-        </ul>
-
         <slot/>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-
+    definePageMeta({
+        middleware: ['auth']
+    })
 </script>
 
 <template>
     <div>
         <h1>Home</h1>
+        <p>Precisa estar autenticado!</p>
     </div>
 </template>
 
